a4a366ebefcb6bcf9bfffe0b2cb624c1dbb83601
unknown <thhri1@student.monash.edu>
2020-01-22 15:13:39 +1100

    cms

---
+++ src/Controller/Admin/CmsController.php
@@ -0,0 +1,16 @@
+<?php
+    namespace App\Controller\Admin;
+
+    use App\Controller\AppController;
+
+    class CmsController extends AppController
+    {
+    	public function index()
+	    {
+
+	    }
+	    public function view()
+	    {
+
+	    }
+    }
